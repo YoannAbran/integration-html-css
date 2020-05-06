4a2718cf09923864ab8377d8ed00a7bca1d48a4c
Yoann Abran <y.abran@codeur.online>
2020-05-06 16:21:26 +0200

    html-->php templates

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+  include 'header.php';
+?>
+
+  <div class="container">
+    <div class="jumbotron">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">Sur ce site, vous retrouverez toutes les informations utiles pour coder une page web.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+    <div class="row">
+      <div class="col-sm mb-2  d-flex justify-content-around  pb-3 ">
+        <div class="card" style="width: 18rem;">
+          <img src="img/HTML5.png" class="card-img-top" alt="HTML5">
+          <div class="card-body">
+            <h5 class="card-title">HTML</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="text-center"><a href="html.html" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm mb-2 d-flex justify-content-around pb-3">
+        <div class="card" style="width: 18rem;">
+          <img src="img/css3.png" class="card-img-top" alt="CSS3">
+          <div class="card-body">
+            <h5 class="card-title">CSS</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+              occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
+            <div class="text-center"><a href="css.html" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm mb-2 d-flex justify-content-around pb-3">
+        <div class="card" style="width: 18rem;">
+          <img src="img/console.png" class="card-img-top" alt="Console de commande">
+          <div class="card-body">
+            <h5 class="card-title">Command line</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="text-center"><a href="console.html" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm mb-2 d-flex justify-content-around pb-3">
+        <div class="card" style="width: 18rem;">
+          <img src="img/gitgithub.png" class="card-img-top" alt="Git et Github">
+          <div class="card-body">
+            <h5 class="card-title">Git & Github</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="text-center"><a href="git.html" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm mb-2 d-flex justify-content-around pb-3">
+        <div class="card" style="width: 18rem;">
+          <img src="img/atom.png" class="card-img-top" alt="Atom">
+          <div class="card-body">
+            <h5 class="card-title">Atom</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="text-center"><a href="atom.html" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm mb-2 d-flex justify-content-around pb-3">
+        <div class="card" style="width: 18rem;">
+          <img src="img/bootstrap.png" class="card-img-top" alt="Boostrap">
+          <div class="card-body">
+            <h5 class="card-title">Bootstrap</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="text-center"><a href="bootstrap.html" class="btn btn-primary">Go somewhere</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php
+    include 'footer.php';
