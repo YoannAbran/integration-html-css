178d338eb5b7dc6baba286e75408719711d95d41
Yoann Abran <y.abran@codeur.online>
2020-05-06 17:03:14 +0200

    modif liens

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><img id="logo-acs" src="img/logo-acs-black.png" alt="logo access code school"></a>
+  <a class="navbar-brand" href=https://www.accesscodeschool.fr/"><img id="logo-acs" src="img/logo-acs-black.png" alt="logo access code school"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
