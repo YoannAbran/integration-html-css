ef03469f23ae02a3cdda462177762d36db02da27
Yoann Abran <y.abran@codeur.online>
2020-05-07 10:04:56 +0200

    navbar column

--- css.php
+++ css.php
@@ -3,7 +3,33 @@
 ?>
 
 <main>
-css3
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Html5</h1>
+        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+      </div>
+    </div>
+  <div class="d-flex"  >
+    <ul class="nav flex-column" id="htmlnavbar">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Introduction</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Bases</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Balises</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Attribut</a>
+      </li>
+    </ul>
+    <div>
+      <h1 id="test">HTML Introduction</h1>
+    </div>
+  </div>
+  
 </main>
 
 <?php
--- index.php
+++ index.php
@@ -11,18 +11,18 @@
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </div>
     <div class="row">
-      <div class="col-sm mb-2  d-flex justify-content-around  pb-3 ">
+      <div class="col-sm mb-2  d-flex justify-content-around ">
         <div class="card" style="width: 18rem;">
           <img src="img/HTML5.png" class="card-img-top" alt="HTML5">
           <div class="card-body">
             <h5 class="card-title">HTML</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <div class="text-center"><a href="html.html" class="btn btn-primary">Go somewhere</a>
+            <div class="text-center"><a href="html.php" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-sm mb-2 d-flex justify-content-around pb-3">
+      <div class="col-sm mb-2 d-flex justify-content-around">
         <div class="card" style="width: 18rem;">
           <img src="img/css3.png" class="card-img-top" alt="CSS3">
           <div class="card-body">
@@ -30,51 +30,51 @@
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
               aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
               occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
-            <div class="text-center"><a href="css.html" class="btn btn-primary">Go somewhere</a>
+            <div class="text-center"><a href="css.php" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-sm mb-2 d-flex justify-content-around pb-3">
+      <div class="col-sm mb-2 d-flex justify-content-around">
         <div class="card" style="width: 18rem;">
           <img src="img/console.png" class="card-img-top" alt="Console de commande">
           <div class="card-body">
             <h5 class="card-title">Command line</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <div class="text-center"><a href="console.html" class="btn btn-primary">Go somewhere</a>
+            <div class="text-center"><a href="console.php" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-sm mb-2 d-flex justify-content-around pb-3">
+      <div class="col-sm mb-2 d-flex justify-content-around">
         <div class="card" style="width: 18rem;">
           <img src="img/gitgithub.png" class="card-img-top" alt="Git et Github">
           <div class="card-body">
             <h5 class="card-title">Git & Github</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <div class="text-center"><a href="git.html" class="btn btn-primary">Go somewhere</a>
+            <div class="text-center"><a href="git.php" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-sm mb-2 d-flex justify-content-around pb-3">
+      <div class="col-sm mb-2 d-flex justify-content-around">
         <div class="card" style="width: 18rem;">
           <img src="img/atom.png" class="card-img-top" alt="Atom">
           <div class="card-body">
             <h5 class="card-title">Atom</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <div class="text-center"><a href="atom.html" class="btn btn-primary">Go somewhere</a>
+            <div class="text-center"><a href="atom.php" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-sm mb-2 d-flex justify-content-around pb-3">
+      <div class="col-sm mb-2 d-flex justify-content-around ">
         <div class="card" style="width: 18rem;">
           <img src="img/bootstrap.png" class="card-img-top" alt="Boostrap">
           <div class="card-body">
             <h5 class="card-title">Bootstrap</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <div class="text-center"><a href="bootstrap.html" class="btn btn-primary">Go somewhere</a></div>
+            <div class="text-center"><a href="bootstrap.php" class="btn btn-primary">Go somewhere</a></div>
           </div>
         </div>
       </div>
